4d7872e68b7df3f5d53fa9df0fda3bfb03002a51
David Simmons <dr.desimmons@gmail.com>
2024-09-14 21:07:40 +0100

    Add initial files for testing

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# gemwallet-dart
+# gemwallet.dart
+
+gemwallet.dart is a Dart package that provides bindings to the [`GemWallet extension`](https://github.com/GemWallet/gemwallet-extension) library, allowing developers to interact with the GemWallet in Dart applications.
+
+## Features
+
+This package is a work in progress, and so does not fully replicate the functionality of `gemwallet-extension`.
+
+
+## Contributing
+Contributions to gemwallet.dart are welcome! To contribute, please fork the repository and submit a pull request. Before submitting a pull request, please make sure that your code follows the [Dart Style Guide](https://dart.dev/guides/language/effective-dart/style).
+
+## license
+gemwallet.dart is licensed under the BSD-3-Clause License. See the LICENSE file for more information.
---
+++ lib/gemwallet.dart
@@ -0,0 +1,101 @@
+@JS('GemWalletApi')
+library gemwallet_api;
+
+import 'package:js/js.dart';
+import 'package:node_interop/util.dart';
+
+class GemWalletApi {
+  static Future<Response> submitTransaction(
+      SubmitTransactionRequest request) async {
+    return await promiseToFuture(_submitTransaction(request));
+  }
+
+  static Future<Response> getAddress() async {
+    return await promiseToFuture(_getAddress());
+  }
+
+  static Future<Response> isInstalled() async {
+    return await promiseToFuture(_isInstalled());
+  }
+
+  static Future<Response> getPublicKey() async {
+    return await promiseToFuture(_getPublicKey());
+  }
+
+  static Future<Response> getNetwork() async {
+    return await promiseToFuture(_getNetwork());
+  }
+
+  static Future<Response> signMessage(String message) async {
+    return await promiseToFuture(_signMessage(message));
+  }
+}
+
+@JS('submitTransaction')
+external dynamic _submitTransaction(SubmitTransactionRequest request);
+
+@JS('getAddress')
+external dynamic _getAddress();
+
+@JS('isInstalled')
+external dynamic _isInstalled();
+
+@JS('getPublicKey')
+external dynamic _getPublicKey();
+
+@JS('getNetwork')
+external dynamic _getNetwork();
+
+@JS('signMessage')
+external dynamic _signMessage(String message);
+
+@JS()
+@anonymous
+class Memo {
+  external factory Memo({
+    String MemoData,
+    String MemoType,
+  });
+}
+
+@JS()
+@anonymous
+class Transaction {
+  external factory Transaction({
+    String? TransactionType,
+    String? Destination,
+    String? Amount,
+    String? Account,
+    String? Channel,
+    String? NFTokenSellOffer,
+    int? SettleDelay,
+    String? PublicKey,
+    List<Memo> Memos,
+  });
+  // Other fields for other types would go here.
+}
+
+@JS()
+@anonymous
+class SubmitTransactionRequest {
+  external factory SubmitTransactionRequest({
+    Transaction transaction,
+  });
+}
+
+@JS()
+@anonymous
+class Response {
+  external Result get result;
+}
+
+@JS()
+@anonymous
+class Result {
+  external String get hash;
+  external String get address;
+  external String get publicKey;
+  external String get network;
+  external String get signedMessage;
+  external bool get isInstalled;
+}
